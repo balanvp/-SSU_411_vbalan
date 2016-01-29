d3ba018c2682663903785668aa173b7dec14cc68
unknown <balanvp@w12-416-23.main.sgu.ru>
2016-01-29 17:52:08 +0300

    FRIDAY111

--- Friday/src/com/company/Main.java
+++ Friday/src/com/company/Main.java
@@ -6,90 +6,43 @@ import java.util.Calendar;
 import java.util.Calendar;
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите год:");
-        int y = sc.nextInt();
-        if (y < 1800 || y > 2020)
+        String scan = sc.nextLine();
 
-            try
-            {
-                System.err.print("ERROR");
-                return;
-            }
-            catch (NumberFormatException e)
-            {
-                System.err.println("Неверный формат года!");
-            }
+        int year = Integer.parseInt(scan);
+        try {
+            if (year < 1800 || year > 2020)
+                System.out.print("ERROR");
 
+        } catch (NumberFormatException e) {
+            System.err.println("Неверный формат года!");
+        }
 
-
-
-        System.out.println(y);
+        System.out.println(year);
         sc.close();
 
         Calendar c = Calendar.getInstance();
-        for (int i = 0; i < 12; ++i)
-
-
-
+        for (int month = 0; month < 12; ++month)
         {
-            c.set(y, i, 13);
+            c.set(year, month, 13);
             if (c.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY) {
 
-
-                if (i == 0) {
-                    System.out.print("Янврь" + "   ");
-                }
-
-                if (i == 1) {
-                    System.out.print("Февраль" + "   ");
-                }
-
-                if (i == 2) {
-                    System.out.print("Март" + "   ");
-                }
-
-                if (i == 3) {
-                    System.out.print("Апрель" + "   ");
-                }
-
-                if (i == 4) {
-                    System.out.print("Май" + "   ");
-                }
-
-                if (i == 5) {
-                    System.out.print("Июнь" + "   ");
-                }
-
-
-                if (i == 6) {
-                    System.out.print("Июль" + "   ");
-                }
-
-                if (i == 7) {
-                    System.out.print("Август" + "   ");
-                }
-
-                if (i == 8) {
-                    System.out.print("Сентябрь" + "   ");
-                }
-
-                if (i == 9) {
-                    System.out.print("Октябрь" + "   ");
-                }
-
-                if (i == 10) {
-                    System.out.print("Ноябрь" + "   ");
-                }
-
-                if (i == 8) {
-                    System.out.print("Декабрь" + "   ");
-                }
-
+                if (month == 0)  {System.out.print("Янврь" + "   ");}
+                if (month == 1) { System.out.print("Февраль" + "   ");}
+                if (month == 2) {System.out.print("Март" + "   ");}
+                if (month== 3) { System.out.print("Апрель" + "   ");}
+                if (month == 4) {System.out.print("Май" + "   ");}
+                if (month == 5) {System.out.print("Июнь" + "   "); }
+                if (month == 6) {System.out.print("Июль" + "   ");}
+                if (month == 7) { System.out.print("Август" + "   ");}
+                if (month == 8) { System.out.print("Сентябрь" + "   ");}
+                if (month == 9) {System.out.print("Октябрь" + "   ");}
+                if (month == 10) {System.out.print("Ноябрь" + "   ");}
+                if (month == 11) {System.out.print("Декабрь" + "   ");}
             }
         }
     }
--- Friday/src/com/company/Month.java
+++ Friday/src/com/company/Month.java
@@ -4,20 +4,16 @@ import java.util.Calendar;
 import java.util.Scanner;
 
 
-public class Month
-{
-    public static void res(int y)
-    {
+public class Month {
+    public static void res(int y) {
 
         Calendar calendar = Calendar.getInstance();
-        calendar.set(calendar.MONTH, y);
+        calendar.set(calendar.MONTH, y-1);
         int quantity = 0;
-        for(int i=1800; i<=2015; i++)
-        {
+        for (int i = 1800; i <= 2015; i++) {
             calendar.set(calendar.YEAR, i);
             calendar.set(calendar.DAY_OF_MONTH, 12);
-            if (calendar.get(calendar.DAY_OF_WEEK)==5)
-            {
+            if (calendar.get(calendar.DAY_OF_WEEK) == 5) {
                 System.out.println(calendar.get(calendar.YEAR));
                 quantity++;
             }
@@ -25,21 +21,17 @@ public class Month
         System.out.println("Количество: " + quantity);
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите месяц: ");
         String sc = scanner.nextLine();
-        try
-        {
+        try {
             int y = Integer.parseInt(sc);
-            if (y >= 0 && y <= 11)
+            if (y >= 1 && y <= 12)
                 res(y);
             else
                 System.err.println("ERROR введите месяц из диапозона");
-        }
-        catch (NumberFormatException e)
-        {
+        } catch (NumberFormatException e) {
             System.err.println("Неправильно");
         }
     }
