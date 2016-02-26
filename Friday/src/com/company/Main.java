caa357dca4a4fe3aac9e74e012a1dce0df4f90ff
unknown <balanvp@w12-416-23.main.sgu.ru>
2016-02-26 16:56:24 +0300

    initial_commit

--- Friday/src/com/company/Main.java
+++ Friday/src/com/company/Main.java
@@ -1,51 +1,201 @@
 package com.company;
+import java.io.*;
+import java.util.*;
 
-import java.time.Month;
-import java.util.Calendar;
+public class Main
+{
+    public static void main(String[] args) throws Exception {
+        BufferedReader d = new BufferedReader(new InputStreamReader(System.in));
+        String sIn, prov="Close";
 
-import java.util.Calendar;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите год:");
-        String scan = sc.nextLine();
-
-        int year = Integer.parseInt(scan);
-        try {
-            if (year < 1800 || year > 2020)
-                System.out.print("ERROR");
-
-        } catch (NumberFormatException e) {
-            System.err.println("Неверный формат года!");
-        }
-
-        System.out.println(year);
-        sc.close();
-
-        Calendar c = Calendar.getInstance();
-        for (int month = 0; month < 12; ++month)
+        BufferedReader res = new BufferedReader(new InputStreamReader(System.in));
+        int y = 100;
+        while (y!=10)
         {
-            c.set(year, month, 13);
-            if (c.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY) {
 
-                if (month == 0)  {System.out.print("Янврь" + "   ");}
-                if (month == 1) { System.out.print("Февраль" + "   ");}
-                if (month == 2) {System.out.print("Март" + "   ");}
-                if (month== 3) { System.out.print("Апрель" + "   ");}
-                if (month == 4) {System.out.print("Май" + "   ");}
-                if (month == 5) {System.out.print("Июнь" + "   "); }
-                if (month == 6) {System.out.print("Июль" + "   ");}
-                if (month == 7) { System.out.print("Август" + "   ");}
-                if (month == 8) { System.out.print("Сентябрь" + "   ");}
-                if (month == 9) {System.out.print("Октябрь" + "   ");}
-                if (month == 10) {System.out.print("Ноябрь" + "   ");}
-                if (month == 11) {System.out.print("Декабрь" + "   ");}
+            try {
+                System.out.println("Введите выражение: ");
+                sIn = d.readLine();
+                if(sIn.equals(prov))
+                {
+                    y = 10;
+                    return;
+                }
+                else
+                {
+                    sIn = opn(sIn);
+                    System.out.println(calculate(sIn));
+                }
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
     }
+
+    /**
+     * Преобразовать строку в обратную польскую нотацию
+     * @param sIn Входная строка
+     * @return Выходная строка в обратной польской нотации
+     */
+    private static String opn(String sIn) throws Exception
+    {
+        StringBuilder sbStack = new StringBuilder("");
+        StringBuilder sbOut = new StringBuilder("");
+        char cIn, cTmp;
+
+        for (int i = 0; i < sIn.length(); i++)
+        {
+            cIn = sIn.charAt(i);
+            if (isOp(cIn))
+            {
+                while (sbStack.length() > 0)
+                {
+                    cTmp = sbStack.substring(sbStack.length()-1).charAt(0);
+                    if (isOp(cTmp) && (opPrior(cIn) <= opPrior(cTmp)))
+                    {
+                        sbOut.append(" ").append(cTmp).append(" ");
+                        sbStack.setLength(sbStack.length()-1);
+                    }
+                    else
+                    {
+                        sbOut.append(" ");
+                        break;
+                    }
+                }
+                sbOut.append(" ");
+                sbStack.append(cIn);
+            }
+            else if ('(' == cIn)
+            {
+                sbStack.append(cIn);
+            }
+            else if (')' == cIn)
+            {
+                cTmp = sbStack.substring(sbStack.length()-1).charAt(0);
+                while ('(' != cTmp)
+                {
+                    if (sbStack.length() < 1)
+                    {
+                        throw new Exception("Ошибка разбора скобок. Проверьте правильность выражения.");
+                    }
+                    sbOut.append(" ").append(cTmp);
+                    sbStack.setLength(sbStack.length()-1);
+                    cTmp = sbStack.substring(sbStack.length()-1).charAt(0);
+                }
+                sbStack.setLength(sbStack.length()-1);
+            }
+            else
+            {
+                // Если символ не оператор - добавляем в выходную последовательность
+                sbOut.append(cIn);
+            }
+        }
+
+        // Если в стеке остались операторы, добавляем их в входную строку
+        while (sbStack.length() > 0)
+        {
+            sbOut.append(" ").append(sbStack.substring(sbStack.length()-1));
+            sbStack.setLength(sbStack.length()-1);
+        }
+        return  sbOut.toString();
+    }
+
+
+    private static boolean isOp(char c)
+    {
+        switch (c)
+        {
+            case '-':
+            case '+':
+            case '*':
+            case '/':
+            case '^':
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Возвращает приоритет операции
+     * @param op char
+     * @return byte
+     */
+    private static byte opPrior(char op)
+    {
+        switch (op)
+        {
+            case '^':
+                return 3;
+            case '*':
+            case '/':
+                return 2;
+        }
+        return 1; // Тут остается + и -
+    }
+
+    /**
+     * Считает выражение, записанное в обратной польской нотации
+     * @param sIn
+     * @return double result
+     */
+    private static double calculate(String sIn) throws Exception
+    {
+        double dA = 0, dB = 0;
+        String sTmp;
+        Deque<Double> stack = new ArrayDeque<Double>();
+        StringTokenizer st = new StringTokenizer(sIn);
+        while(st.hasMoreTokens())
+        {
+            try
+            {
+                sTmp = st.nextToken().trim();
+                if (1 == sTmp.length() && isOp(sTmp.charAt(0)))
+                {
+                    if (stack.size() < 2)
+                    {
+                        throw new Exception("Неверное количество данных в стеке для операции " + sTmp);
+                    }
+                    dB = stack.pop();
+                    dA = stack.pop();
+                    switch (sTmp.charAt(0))
+                    {
+                        case '+':
+                            dA += dB;
+                            break;
+                        case '-':
+                            dA -= dB;
+                            break;
+                        case '/':
+                            dA /= dB;
+                            break;
+                        case '*':
+                            dA *= dB;
+                            break;
+                        case '^':
+                            dA = Math.pow(dA, dB);
+                            break;
+                        default:
+                            throw new Exception("Недопустимая операция " + sTmp);
+                    }
+                    stack.push(dA);
+                }
+                else
+                {
+                    dA = Double.parseDouble(sTmp);
+                    stack.push(dA);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Недопустимый символ в выражении");
+            }
+        }
+
+        if (stack.size() > 1)
+        {
+            throw new Exception("Количество операторов не соответствует количеству операндов");
+        }
+        return stack.pop();
+    }
 }
-
-
